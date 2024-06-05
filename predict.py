1587eb21bc98009b7ba315d50653faf183db99cf
WAILCH6 <guez660@gmail.com>
2024-06-05 05:17:50 +0100

    Initial commit

--- predict.py
+++ predict.py
@@ -1,14 +1,22 @@
 import sys
 import joblib
 import json
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 def predict(model_path, heure):
     model = joblib.load(model_path)
     prediction = model.predict([[int(heure)]])
     return prediction[0]
 
-if __name__ == '__main__':
-    model_path = sys.argv[1]
-    heure = sys.argv[2]
+@app.route('/api/predict', methods=['POST'])
+def predict_route():
+    data = request.get_json()
+    heure = data['heure']
+    model_path = 'random_forest_model.pkl'  # Assurez-vous que ce chemin est correct
     prediction = predict(model_path, heure)
-    print(json.dumps({'prediction': prediction}))
+    return jsonify({'prediction': prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
